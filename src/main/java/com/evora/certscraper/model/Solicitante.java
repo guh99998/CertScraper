d6f7b1a6102ecabe72dd8df1ce9e86e4901dbaf9
Gustavo Lopes <guhrlopes999@gmail.com>
2026-06-11 17:08:42 -0300

    feat: criado model Solicitante.java

---
+++ src/main/java/com/evora/certscraper/model/Solicitante.java
@@ -0,0 +1,53 @@
+package com.evora.certscraper.model;
+
+public class Solicitante {
+    private String nome;
+    private String cpf;
+    private String email;
+    private Endereco endereco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public Solicitante(String nome, String cpf, String email, Endereco endereco) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.endereco = endereco;
+    }
+
+    public Solicitante(String nome, String cpf, String email) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+    }
+}
