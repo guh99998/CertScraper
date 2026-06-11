b516a0a8b929d8a60068301b21fe43768ce9c57f
Gustavo Lopes <guhrlopes999@gmail.com>
2026-06-11 17:08:32 -0300

    feat: criado exports para controller

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,8 @@ module com.evora.certscraper {
     requires javafx.controls;
     requires javafx.fxml;
 
-
-    opens com.evora.certscraper to javafx.fxml;
     exports com.evora.certscraper;
+    exports com.evora.certscraper.controller;
+
+    opens com.evora.certscraper.controller to javafx.fxml;
 }
